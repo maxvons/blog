10a97b62110356374ab5471915b273e8d6d503f3
Maximilian von Stephanides <maximilianvons@gmail.com>
2021-11-04 23:07:34 +0100

    Make the landing page responsive
    - Made the `index` page responsive.
    - Made the `Landing` component responsive.
    - Made the `Card` component responsive.
    - Made the `CardDark` component responsive.

--- gatsby_blog/src/components/CardDark.tsx
+++ gatsby_blog/src/components/CardDark.tsx
@@ -11,19 +11,15 @@ interface CardDarkProps {
   heading: string;
   excerpt: string;
   path: string;
-  rightMargin?: boolean;
 }
 
 const CardDark = ({
   heading = "Check out all my blog posts",
   excerpt = "Technical articles, gear reccomendations, fun stuff I’ve learned, and more.",
   path = "/learn-javascript",
-  rightMargin,
 }: CardDarkProps) => {
-  const classes = `card-dark ${rightMargin && "right-margin-large"}`;
-
   return (
-    <Link to={path} className={classes}>
+    <Link to={path} className="card-dark">
       <h2 className="card-heading-dark">{heading}</h2>
       <p className="excerpt-dark">{excerpt}</p>
     </Link>
